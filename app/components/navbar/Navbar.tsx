8aa27af9f603ef7542f85f6a34c0167ac3228d7a
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-05-12 06:02:15 +0300

    made the homepage and projects page responsive

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from "../../../public/signature.png"
 
+
 const Navbar = () => {
+  const [ isMenuOpen, setIsMenuOpen ] = React.useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-      <div className='navbar'>
-          <div className="logo">
-          <a href="/">
-              <img src={logo} alt="logo" />
-          </a>
-          </div>
-          <div className="links">
-              <a href="/services"> My Services</a>
-              <a href="/projects">Projects & Code</a>
-          </div>
-          <div className="call-btn">
-              <a href="https://cal.com/ahmed-sherif-tg2x15" target='_blank'>Schedule A Call</a>
-          </div>
+    <div className='navbar'>
+      <div className="logo">
+        <a href="/">
+          <img src={logo} alt="logo" />
+        </a>
       </div>
+      <div className="burger-menu" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <div className={`links ${isMenuOpen ? 'open' : ''}`}>
+        <a href="/services"> My Services</a>
+        <a href="/projects">Projects & Code</a>
+      </div>
+      <div className="call-btn">
+        <a href="https://cal.com/ahmed-sherif-tg2x15" target='_blank'>Schedule A Call</a>
+      </div>
+    </div>
 
   )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/navbar/Navbar";
 import Break from "../components/break/Break";
-import image from "../../public/favicon.ico"
+import image from "../../public/favicon.ico";
 import book from "../../public/book.png";
 import weight from "../../public/weight.png";
 import tag from "../../public/tag.png";
@@ -17,7 +17,7 @@ import instagram from "../../public/instagram.png";
 import whatsapp from "../../public/whatsapp.png";
 import FloatingBackground from "../components/FloatingBackground/FloatingBackground";
 import mine from "../../public/x.png";
-export function meta({ }: Route.MetaArgs) {
+export function meta({}: Route.MetaArgs) {
   return [
     { title: "Shefo's App" },
     { name: "description", content: "Shefo's Portfolio app" },
@@ -32,16 +32,21 @@ export default function Home() {
         <Navbar />
         <div className="main flex-center">
           <div className="main-h1">
-            <h1>
-              Hey, I'm Ahmed Sherif
-            </h1>
-            <h2>I’m a senior <strong>React.js developer , </strong>
-              creates <strong>responsive</strong> web apps</h2>
+            <h1>Hey, I'm Ahmed Sherif</h1>
+            <h2>
+              I’m a senior <strong>React.js developer , </strong>
+              creates <strong>responsive</strong> web apps
+            </h2>
             <div className="call-btn view-projects">
               <a href="/projects">View projects</a>
             </div>
             <div className="call-btn resume">
-              <a href="https://drive.google.com/file/d/1EuW1V634lJ3vUq6rT7SKC-YUXU7_loIU/view?usp=drive_link" target='_blank'>Download Resume</a>
+              <a
+                href="https://drive.google.com/file/d/1EuW1V634lJ3vUq6rT7SKC-YUXU7_loIU/view?usp=drive_link"
+                target="_blank"
+              >
+                Download Resume
+              </a>
             </div>
           </div>
           <div className="main-img">
@@ -49,41 +54,45 @@ export default function Home() {
           </div>
         </div>
       </div>
-<Break />
-
-
-
-
-
-
+      <Break />
 
       <div className="facts">
-        <div className="facts-img"><img src={me} alt="my first image" className="me1" />
+        <div className="facts-img">
+          <img src={me} alt="my first image" className="me1" />
           <img src={me2} alt="my second img" className="me2" />
         </div>
         <div className="facts-txt">
-          <h1>Quick Facts About Me!</h1><br />
-          <p >I craft sleek, high-performance web interfaces with React.js | turning ideas into interactive, user-friendly experiences that bring your vision to life.</p>
+          <h1>Quick Facts About Me!</h1>
+          <br />
+          <p>
+            I craft sleek, high-performance web interfaces with React.js |
+            turning ideas into interactive, user-friendly experiences that bring
+            your vision to life.
+          </p>
           <ul className="facts-list">
-            <li >
-              <img src={book} alt="book-icon" /> <p> Studied computer Science on my own</p>
+            <li>
+              <img src={book} alt="book-icon" />{" "}
+              <p> Studied computer Science on my own</p>
             </li>
             <li>
-              <img src={browser} alt="browser-icon" /><p> Started building front-end as a hobby but it turned to a job</p>
+              <img src={browser} alt="browser-icon" />
+              <p>
+                {" "}
+                Started building front-end as a hobby but it turned to a job
+              </p>
             </li>
             <li>
-              <img src={tag} alt="tag-icon" /><p>Love Turning Ideas into life through coding</p>
+              <img src={tag} alt="tag-icon" />
+              <p>Love Turning Ideas into life through coding</p>
             </li>
             <li>
-              <img src={weight} alt="weight-icon" /><p>When I’m not coding I’m lifting at the GYM</p>
+              <img src={weight} alt="weight-icon" />
+              <p>When I’m not coding I’m lifting at the GYM</p>
             </li>
           </ul>
         </div>
       </div>
       <div className="call-div">
-
-
-
         <div className="call-txt">
           <div className="call-text">
             <h1>Ready To get your business to the next Level?!</h1>
@@ -102,13 +111,18 @@ export default function Home() {
               <img src={customer4} alt="customer-4" />
             </div>
           </div>
-          <div className="call-image"> <img src={mine} alt="my-photo" /></div>
+          <div className="call-image">
+            {" "}
+            <img src={mine} alt="my-photo" />
+          </div>
         </div>
 
-
-
         <div className="call-btn">
-          <a href="https://cal.com/ahmed-sherif-tg2x15" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://cal.com/ahmed-sherif-tg2x15"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Schedule A Call
           </a>
         </div>
@@ -118,22 +132,23 @@ export default function Home() {
         <h1>Ready To make your dream come true?!</h1>
         <p>Just Contact me to level up your business</p>
         <div className="btn-container">
-          <div className="call-btn"><a href="https://www.instagram.com/ishefo0" target="_blank">
-            <div className="call-btnn">
-              <img src={instagram} alt="" className="call-imgg" /> Instagram
-            </div>
-          </a></div>
-          <div className="call-btn"><a href="https://wa.me/+201274375560" target="_blank">
-            <div className="call-btnn">
-              <img src={whatsapp} alt="" className="call-imgg" />Whatsapp
-            </div></a></div>
-
+          <div className="call-btn">
+            <a href="https://www.instagram.com/ishefo0" target="_blank">
+              <div className="call-btnn">
+                <img src={instagram} alt="" className="call-imgg" /> Instagram
+              </div>
+            </a>
+          </div>
+          <div className="call-btn">
+            <a href="https://wa.me/+201274375560" target="_blank">
+              <div className="call-btnn">
+                <img src={whatsapp} alt="" className="call-imgg" />
+                Whatsapp
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-
-
-
-
     </div>
   );
 }
